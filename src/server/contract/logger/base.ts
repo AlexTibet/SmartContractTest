c95531cf56cbdfdf152719d87cecc5dfc32f90a8
Alexandr Sobko <alexfreetibet@gmail.com>
2021-12-21 02:11:19 +0700

    create deposit event handle

--- src/server/contract/logger/base.ts
+++ src/server/contract/logger/base.ts
@@ -1,12 +1,36 @@
 import { EventData } from 'web3-eth-contract';
+import { ITransactionInfo } from '../../models/Transactions';
+import BigNumber from 'bignumber.js';
 
 export abstract class EventLogger {
   error: unknown;
-  event: EventData;
+  readonly event: EventData;
+  private readonly transactionInfo: ITransactionInfo
 
   constructor(err: unknown, event: EventData) {
     this.error = err;
     this.event = event;
+
+    if (!err) {
+      this.transactionInfo = this.parseEventData();
+    }
+  }
+
+  protected parseEventData(): ITransactionInfo {
+    return {
+      from: this.event.returnValues['0'],
+      to: this.event.returnValues['2'],
+      amount: new BigNumber(this.event.returnValues['1']),
+      removed: this.event['removed'],
+      blockNumber: this.event.blockNumber,
+      blockHash: this.event.blockHash,
+      logIndex: this.event.logIndex,
+      transactionIndex: this.event.transactionIndex,
+      transactionHash: this.event.transactionHash,
+      returnValues: this.event.returnValues,
+      signature: this.event.signature,
+      rawData: this.event.raw
+    };
   }
 
   async errorHandler(): Promise<void> {
@@ -14,5 +38,9 @@ export abstract class EventLogger {
     console.log('\t', this.error);
   }
 
-  abstract execute(err, data: EventData): Promise<void>
+  abstract execute(): Promise<void>
+
+  getTransactionInfo(): ITransactionInfo {
+    return this.transactionInfo;
+  }
 }
--- src/server/contract/logger/deposit.ts
+++ src/server/contract/logger/deposit.ts
@@ -1,11 +1,72 @@
 import { EventLogger } from './base';
+import { Users } from '../../models/Users';
+import { Transactions, TRANSACTIONS_TYPE } from '../../models/Transactions';
+import sequelize from '../../models';
+import { Wallets } from '../../models/Wallets';
 
 export class DepositLogger extends EventLogger {
   async execute(): Promise<void> {
     if (this.error) {
       return await this.errorHandler();
     }
+
     console.log('DEPOSIT EVENT');
     console.log('\t', this.event);
+
+    const transactionInfo = this.getTransactionInfo();
+    console.log(transactionInfo);
+
+    const t = await sequelize.transaction();
+    try {
+      const [transaction, trxNotFound] = await Transactions.findOrCreate({
+        where: {
+          transactionHash: transactionInfo.transactionHash
+        },
+        defaults: {
+          ...transactionInfo,
+          type: TRANSACTIONS_TYPE.DEPOSIT
+        },
+        transaction: t
+      });
+
+      if (!trxNotFound) {
+        console.log('FIND', transaction.transactionHash);
+        return;
+      }
+
+      const [user] = await Users.findOrCreate({
+        where: {
+          address: transactionInfo.from
+        },
+        defaults: {
+          address: transactionInfo.from
+        },
+        transaction: t
+      });
+
+      const [wallet] = await Wallets.findOrCreate({
+        where: {
+          userId: user.id,
+          token: transactionInfo.to
+        },
+        defaults: {
+          userId: user.id,
+          token: transactionInfo.to
+        },
+        transaction: t
+      });
+
+      await wallet.increment(
+          'balance',
+          {
+            by: transactionInfo.amount.toNumber(),
+            transaction: t
+          });
+
+      await t.commit();
+    } catch (err) {
+      console.log(err);
+      await t.rollback();
+    }
   }
 }
